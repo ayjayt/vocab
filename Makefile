9f4797d19b0a1cf66096eebf631d766f40b7a3ef
Andrew Pikul <ajpikul@gmail.com>
2020-10-25 20:03:48 -0400

    Make build, format with newlines, dont hardcode keys

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 export GOOGLE_APPLICATION_CREDENTIALS=$(PWD)/keys/vocab-32f12887b205.json
 all:
-	go build && ./vocab -map maps/ayjay_t -input raw/gibberish.txt
+	go build && ./vocab -input test_input/gibberish.txt
--- import.go
+++ import.go
@@ -3,7 +3,7 @@ package main
 
 import (
 	"io/ioutil"
-	//"os"
+	"os"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -15,24 +15,19 @@ import (
 	"cloud.google.com/go/translate"
 	"golang.org/x/text/language"
 	"github.com/sthorne/go-hunspell"
-
-
-
 )
-
 
 var inputFileString = flag.String("input", "", "Specify the file you want to input")
 var homeLanguageString = flag.String("home", "en", "Specify your language")
 var awayLanguageString = flag.String("away", "es", "Specify desired language")
-var inputLanguageString = flag.String("input", "es,en", "Specify the languages int he document")
+var inputLanguageString = flag.String("input_languages", "es,en", "Specify the languages int he document")
 var wordRegexp = regexp.MustCompile(`\s*['\-\pL]['\-\pL]['\-\pL]+\s*`)
 
 func init() {
 	flag.Parse()
 }
 
-// This is basically to convert your english word to spanish
-// It probably needs to be an interface
+// GoogleWord will translate a word TODO: can batch
 func GoogleWord(client *translate.Client, ctx context.Context, word, inLanguageString, outLanguageString string) string { // convert input language to the destination language
 		// Google Translate
 		outLanguage, err := language.Parse(outLanguageString)
@@ -45,11 +40,11 @@ func GoogleWord(client *translate.Client, ctx context.Context, word, inLanguageS
 		})
 		check(err)
 		if len(TranslateResp) == 0 {
-			fmt.Printf("Returned empty on translation")
+			fmt.Printf("Returned empty on translation\n")
 		} else {
 			fmt.Printf("resp:\n%+v\n", TranslateResp)
 		}
-		return "" // TODO: fix return, parse this
+		return strings.ToLower(TranslateResp[0].Text)
 }
 
 type OxfordClient struct {
@@ -60,8 +55,13 @@ type OxfordClient struct {
 func (client OxfordClient) OxfordDefine(word string) []string {
 	fmt.Printf("Requesting:https://od-api.oxforddictionaries.com/api/v2/lemma/es/" + word + "\n")
 	req, err := http.NewRequest("GET", "https://od-api.oxforddictionaries.com/api/v2/entries/es/" + word, nil)
-	req.Header.Add("app_id", ``)
-	req.Header.Add("app_key", ``) // TODO: fix get keys out
+	OXFORD_ID := os.Getenv("OXFORD_ID")
+	OXFORD_PASS := os.Getenv("OXFORD_PASS")
+	if (len(OXFORD_ID) == 0 || len(OXFORD_PASS) == 0) {
+		panic("I need key headers OXFORD_ID and OXFORD_PASS")
+	}
+	req.Header.Add("app_id", OXFORD_ID)
+	req.Header.Add("app_key", OXFORD_PASS) // TODO: fix get keys out
 	resp, err := client.Do(req)
 	check(err)
 	jsonData := make(map[string]interface{})
@@ -69,7 +69,7 @@ func (client OxfordClient) OxfordDefine(word string) []string {
 		json.NewDecoder(resp.Body).Decode(&jsonData)
 		b, err := json.MarshalIndent(jsonData, "", "  ")
 		check(err)
-		return []string{string(b)}
+		return []string{string(b)} // TODO: still returning whole response
 	}
 	return nil
 }
@@ -93,12 +93,11 @@ func main() {
 
 	// These are essentially the results
 	var wordMap = make(map[string][]Vocab, 0)
-
 	// Setting up the dictionaries- not sure if they're thread safe
 	dictionaries := make(map[string]*hunspell.Hunhandle, 2)
 	// All dictionaries will be cached
-	dictionaries["es"] = hunspell.Hunspell("resources/hunspell_dictionaries/es_ES.aff", "hunspell_dictionaries/es_ES.dic")
-	dictionaries["en"] = hunspell.Hunspell("resources/hunspell_dictionaries/en_US.aff", "hunspell_dictionaries/en_US.dic")
+	dictionaries["es"] = hunspell.Hunspell("resources/hunspell_dictionaries/es_ES.aff", "resources/hunspell_dictionaries/es_ES.dic")
+	dictionaries["en"] = hunspell.Hunspell("resources/hunspell_dictionaries/en_US.aff", "resources/hunspell_dictionaries/en_US.dic")
 
 	inputLanguages := strings.Split(*inputLanguageString,",")
 	for _, v := range inputLanguages {
@@ -121,21 +120,31 @@ func main() {
 
 	// Iterate over words
 	for index, _ := range wordMapRaw {
-		fmt.Printf("\n\n%v\n", index)
-
+		fmt.Printf("--%v\n", index)
 		// Convert all input languages to the away language
 		for _, lang := range inputLanguages {
 			if (dictionaries[lang].Spell(index)) {
 				if (lang != *awayLanguageString) {
-					GoogleWord(googleClient, ctx, index, lang, *awayLanguageString)
-					// Lets store how much we're googling
+					fmt.Printf("Translating\n")
+					translation := GoogleWord(googleClient, ctx, index, lang, *awayLanguageString)
+					fmt.Printf("Translated\n")
+					if (len(translation) > 0) {
+						index = translation
+					} else {
+						continue
+					}
 				}
+				fmt.Printf("Going to lemmatize and add it to the wordMap\n")
 				if _, ok := wordMap[index]; !ok { // TODO maybe we should lookup the word
 					// Now we need to lemmatize it TODO
+					// check to see if the lemmatize version is in wordmap TODO
+					fmt.Printf("Adding: %v\n", index)
 					wordMap[index] = make([]Vocab,0) // TODO do we even need to create a wordMap like this?
 					// We're checking dictionary TODO
 					// Adding it if it's not there TODO
 					// And adding it to the deck if it is now or was TODO
+				} else {
+					fmt.Printf("Actually, it was already present\n")
 				}
 			}
 		}
@@ -143,8 +152,9 @@ func main() {
 	}
 	// We're going to get the base for each word now
 		// Oxford Translate and Dump
-		definition := oxfordClient.OxfordDefine("") // TODO: this should be a base
-		_ = definition
+	//	definition := oxfordClient.OxfordDefine("") // TODO: this should be a base
+	_ = oxfordClient
+
 }
 
 func check (e error) {
