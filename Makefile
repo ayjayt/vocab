8fd4d4763a0aaea81360bab6eb964cced1e9fc3e
Andrew Pikul <ajpikul@gmail.com>
2020-12-07 14:38:17 -0600

    Add Ultralingua API and sanitized-text intermediate output

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 export GOOGLE_APPLICATION_CREDENTIALS=$(PWD)/keys/vocab-32f12887b205.json
+
 all:
 	go build && ./vocab -input test_input/gibberish.txt
--- import.go
+++ import.go
@@ -20,8 +20,10 @@ import (
 var inputFileString = flag.String("input", "", "Specify the file you want to input")
 var homeLanguageString = flag.String("home", "en", "Specify your language")
 var awayLanguageString = flag.String("away", "es", "Specify desired language")
-var inputLanguageString = flag.String("input_languages", "es,en", "Specify the languages int he document")
-var wordRegexp = regexp.MustCompile(`\s*['\-\pL]['\-\pL]['\-\pL]+\s*`)
+//var inputLanguageString = flag.String("input_languages", "es,en", "Specify the languages int he document")
+var inputLanguageString = flag.String("input_languages", "es", "Specify the languages int he document")
+var wordRegexp = regexp.MustCompile(`\s*['\-\pL]['\-\pL]['\-\pL]+[,.!?;]*\s*`)
+var barewordRegexp = regexp.MustCompile(`\s*['\-\pL]['\-\pL]['\-\pL]+\s*`)
 
 func init() {
 	flag.Parse()
@@ -47,6 +49,49 @@ func GoogleWord(client *translate.Client, ctx context.Context, word, inLanguageS
 		return strings.ToLower(TranslateResp[0].Text)
 }
 
+type UltraLinguaClient struct {
+	http.Client
+}
+
+// UltraLinguaClient gets a response about definitions from an oxford dictionary
+func (client UltraLinguaClient) ULLemma(word string) []string {
+	fmt.Printf("https://api.ultralingua.com/api/2.0/lemmas/es/"+word+"?key=PLPPRTU8J8QE3BDPJAL6XDBN\n")
+	req, err := http.NewRequest("GET", "https://api.ultralingua.com/api/2.0/lemmas/es/"+word+"?key=PLPPRTU8J8QE3BDPJAL6XDBN", nil)
+	check(err)
+	resp, err := client.Do(req)
+	check(err)
+	defer resp.Body.Close()
+	var jsonData interface{}
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		check(err)
+		//json.NewDecoder(resp.Body).Decode(&jsonData)
+		//b, err := json.MarshalIndent(jsonData, "", "  ")
+		err = json.Unmarshal(bodyBytes, &jsonData)
+		//check(err)
+		//fmt.Printf("jsonData: %+v\n", jsonData)
+		// Lets go through this map and find all "root"
+		fmt.Printf("Body Bytes:\n%+v\n", string(bodyBytes))
+		fmt.Printf("JSON Data: \n%+v\n", jsonData)
+		roots := make(map[string]bool,0)
+		// TODO: needs to deeper
+		for _, val := range jsonData.([]interface{}) {
+			if valMap, ok := val.(map[string]interface{}); ok {
+				root, ok := valMap["root"]; if ok {
+					fmt.Printf("Found: %v\n", string(root.(string)))
+					roots[string(root.(string))] = true
+				}
+			}
+		}
+// loop through json data
+		keys := make([]string, 0, len(roots))
+		for k := range roots {
+				keys = append(keys, k)
+		}
+		return keys// TODO: still returning whole response
+	}
+	return nil
+}
 type OxfordClient struct {
 	http.Client
 }
@@ -74,19 +119,19 @@ func (client OxfordClient) OxfordDefine(word string) []string {
 	return nil
 }
 
-// ReadFile right now reads a global file
-func ReadFile() map[string]bool {
+// ReadFile right now reads a global file var set by flag- for inputs
+func ReadFile() (map[string]bool, []string) {
 	inputText, err := ioutil.ReadFile(*inputFileString)
 	check(err)
 	allWordsFromRaw := wordRegexp.FindAllString(string(inputText), -1)
 	if allWordsFromRaw == nil {
-		return nil
+		return nil, nil
 	}
 	wordMapRaw := make(map[string]bool, len(allWordsFromRaw))
 	for _, val := range allWordsFromRaw {
 		wordMapRaw[strings.ToLower(strings.TrimSpace(val))] = true
 	}
-	return wordMapRaw
+	return wordMapRaw, allWordsFromRaw
 }
 
 func main() {
@@ -106,6 +151,7 @@ func main() {
 
 	// This is an http client for oxford's api. Probably going to take a context too TODO. 
 	oxfordClient := &OxfordClient{}
+	ultraLinguaClient := &UltraLinguaClient{}
 
 	// This is the setup for google translate API
 	ctx := context.Background()
@@ -116,28 +162,54 @@ func main() {
 	defer googleClient.Close()
 
 	// Start work
-	wordMapRaw := ReadFile()
+	wordMapRaw, wordSlice := ReadFile() // a map of potential words to a bool
+	wholeTextSanitized := make([]string, 0, len(wordSlice))
+	for _, word := range wordSlice {
+		bareword := strings.TrimSpace(barewordRegexp.FindString(word))
+		for _, lang := range inputLanguages {
+			if (dictionaries[lang].Spell(bareword)) { // if it is a properly spelled word in some language
+				if (lang != *awayLanguageString) {
+					continue
+				}
+				wholeTextSanitized = append(wholeTextSanitized, word)
+			}
+		}
+	}
+	fmt.Printf("Whole text sanitized: \n%v\n", wholeTextSanitized)
 
 	// Iterate over words
 	for index, _ := range wordMapRaw {
 		fmt.Printf("--%v\n", index)
 		// Convert all input languages to the away language
 		for _, lang := range inputLanguages {
-			if (dictionaries[lang].Spell(index)) {
-				if (lang != *awayLanguageString) {
+			if (dictionaries[lang].Spell(index)) { // if it is a properly spelled word in some language
+				if (lang != *awayLanguageString) { // if it's an input other than the language we're learning (then do a weak translation to what we're learning)
 					fmt.Printf("Translating\n")
-					translation := GoogleWord(googleClient, ctx, index, lang, *awayLanguageString)
+					translation := GoogleWord(googleClient, ctx, index, lang, *awayLanguageString) // translate it
 					fmt.Printf("Translated\n")
 					if (len(translation) > 0) {
 						index = translation
 					} else {
-						continue
+						continue // translation yielded nothing, continue as if if dictionaries[lang].Spell() == False
 					}
 				}
 				fmt.Printf("Going to lemmatize and add it to the wordMap\n")
 				if _, ok := wordMap[index]; !ok { // TODO maybe we should lookup the word
-					// Now we need to lemmatize it TODO
+					// HARDCODING SPANSIH
+					// HARDCODING KEY ?key=PLPPRTU8J8QE3BDPJAL6XDBN
+					//definition := ultraLinguaClient.ULLemma(index) // TODO: this should be a base
+					//fmt.Printf("Lemmas:\n%+v\n", definition)
+					_= ultraLinguaClient
+					// Now we need to lemmatize it TODnPO
 					// check to see if the lemmatize version is in wordmap TODO
+
+					// so now we have the index
+					// and we have the lemmas
+
+					// we should make a map of indices-->bases
+					// bases--> definitions
+
+
 					fmt.Printf("Adding: %v\n", index)
 					wordMap[index] = make([]Vocab,0) // TODO do we even need to create a wordMap like this?
 					// We're checking dictionary TODO
@@ -162,4 +234,5 @@ func check (e error) {
 		panic(e)
 	}
 }
+
 
